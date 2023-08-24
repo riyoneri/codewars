15314be893e93e3ae2f5421bfe58272af64bcd7e
Lionel Kaneza <lionkaneza@gmail.com>
2023-08-24 10:43:24 +0200

    feat: add amount-of-subsets.ts[6 kyu]

---
+++ TypeScript/6 kyu/amount-of-subsets.ts
@@ -0,0 +1,6 @@
+export function estSubsets<T>(list: T[]): number {
+   let length = [...new Set(list)].length
+  return length == 0 ? 0 : Math.pow(2, length) - 1;
+}
+
+console.log(estSubsets([2, 3, 4, 5, 5, 6, 6, 7, 8, 8]));
