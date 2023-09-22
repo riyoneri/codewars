835a7fe57879deeef47fc041a6b4cb52fe9c554d
Lionel Kaneza <lionkaneza@gmail.com>
2023-09-22 10:53:44 +0200

    refactor: add steps in i-love-big-nums.js[6 kyu]

---
+++ JavaScript/5 kyu/play-with-string.js
@@ -0,0 +1,7 @@
+function workOnStrings(a, b) {
+  //..
+}
+
+// If you meet character in b which exists in a change its case
+// Reverse inputs and then follow the same formula
+// Return the string consisting of the changed version of a followed by changed version of b
--- JavaScript/6 kyu/i-love-big-nums.js
+++ JavaScript/6 kyu/i-love-big-nums.js
@@ -3,6 +3,10 @@ function biggest(nums) {
   return nums.sort((a, b) => (`${a}${b}` > `${b}${a}` ? -1 : 1)).join("");
 }
 
+// check all numbers if are 0 and then return 0
+// sort numbers based on how they follow each other ab > ba return -1
+// join numbers with ""
+
 console.log(biggest([0, 0, 0, 0]));
 console.log(biggest([121, 12]));
 console.log(biggest([3, 30, 34, 5, 9]));
